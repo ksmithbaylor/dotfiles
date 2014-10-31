a8a8a7e93183e1fcf494b324962a4e1737b4bb82
Kevin Smith <ksmithbaylor@gmail.com>
2014-10-30 20:47:40 -0400

    default homebrew cask to symlink into rood /Applications folder

--- bash/scripts/exports.sh
+++ bash/scripts/exports.sh
@@ -1,6 +1,7 @@
 export EDITOR=vim
 export HISTFILESIZE=10000
 export CLICOLOR=true
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 if which rbenv > /dev/null; then
     export RBENV_ROOT=/usr/local/var/rbenv
