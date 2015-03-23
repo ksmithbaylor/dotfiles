30a5757b73d971c5251382c2081aa6b6e083abdf
Kevin Smith <ksmithbaylor@gmail.com>
2015-03-23 00:06:56 -0400

    added lein-exec

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-swank "1.4.5"]
                   [cider/cider-nrepl "0.7.0"]
-                  [chestnut/lein-template "0.6.0"]]
+                  [chestnut/lein-template "0.6.0"]
+                  [lein-exec "0.3.4"]]
         :dependencies [[net.mikera/core.matrix "0.29.1"]]}}
