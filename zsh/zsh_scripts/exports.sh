6c659dfc30b787bd82bff7ab37cfe26c98d564fa
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-21 14:58:29 -0400

    add docker environment variables

--- zsh/zsh_scripts/exports.sh
+++ zsh/zsh_scripts/exports.sh
@@ -4,6 +4,11 @@ export CLICOLOR=true
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export ANDROID_HOME=$(brew --prefix android-sdk)
 
+# Docker
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/kevin/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+
 if command_exists rbenv; then
     export RBENV_ROOT=/usr/local/var/rbenv
     eval "$(rbenv init - zsh)"
