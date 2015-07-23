6ba5464ae6183dff18e4996437746fd0345369cf
Kevin Smith <ksmithbaylor@gmail.com>
2015-07-23 18:59:17 -0400

    fix behavior of delete, home, and end

--- zsh/zsh_scripts/util.sh
+++ zsh/zsh_scripts/util.sh
@@ -19,3 +19,8 @@ done
 eval _Reset='%{$reset_color%}'
 
 bindkey -e
+
+# correctly bind delete, home, and end keys
+bindkey "^[[3~" delete-char
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
