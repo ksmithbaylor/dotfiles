c6caba1d03e876a2be90b19024e9af3befc7b82d
Kevin Smith <ksmithbaylor@gmail.com>
2016-07-31 16:51:02 -0400

    More changes to emacs config, not sure what they do

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,11 +1,11 @@
 ; Load my customizations
 (add-to-list 'load-path
-	     (expand-file-name "customizations"
-			       user-emacs-directory))
+             (concat user-emacs-directory
+                     (convert-standard-filename "customizations/")))
 
 ; General editor config and customization
 (require 'init-packages)
-(require 'init-evil)
+;(require 'init-evil)
 (require 'init-theme)
 (require 'init-editing)
 (require 'init-settings)
