6aad3fcd8f8baab7a92c09ca49ace24cf47825b7
Kevin Smith <ksmithbaylor@gmail.com>
2016-08-16 09:57:09 -0400

    Colorize man pages

--- .scripts/common/aliases.sh
+++ .scripts/common/aliases.sh
@@ -77,6 +77,19 @@ function mdcd {
     cd "$1"
 }
 
+# Colorized man pages from https://gist.github.com/cocoalabs/2fb7dc2199b0d4bf160364b8e557eb66
+function man {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+      man "$@"
+}
+
 function repeatedly {
     local interval="$1" command="$2"
     while [ true ]; do
