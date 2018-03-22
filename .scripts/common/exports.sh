5c60fe9325f20bfd23e865b6079c466660340e4c
Kevin Smith <ksmithbaylor@gmail.com>
2018-03-22 01:30:31 -0400

    comment out some unused init stuff

--- .scripts/common/exports.sh
+++ .scripts/common/exports.sh
@@ -2,7 +2,7 @@ export EDITOR=nvim
 export HISTFILESIZE=10000
 export CLICOLOR=true
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-is_mac && command_exists brew && command_exists android && export ANDROID_HOME=$(brew --prefix android-sdk)
+# is_mac && command_exists brew && command_exists android && export ANDROID_HOME=$(brew --prefix android-sdk)
 
 if command_exists rbenv; then
     export RBENV_ROOT=/usr/local/var/rbenv
@@ -18,10 +18,10 @@ if command_exists pair; then
   eval "$(pair --setup)"
 fi
 
-if command_exists opam; then
-  eval $(opam config env)
-fi
+# if command_exists opam; then
+  # eval $(opam config env)
+# fi
 
-if [ -f /usr/libexec/java_home ]; then
-  export JAVA_HOME=$(/usr/libexec/java_home)
-fi
+# if [ -f /usr/libexec/java_home ]; then
+  # export JAVA_HOME=$(/usr/libexec/java_home)
+# fi
