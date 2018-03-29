1464a2753e35e443aca9d569fb903841609bbbd3
Kevin Smith <ksmithbaylor@gmail.com>
2018-03-29 14:27:37 -0400

    Fix ruby zsh init stuff

--- .scripts/common/exports.sh
+++ .scripts/common/exports.sh
@@ -4,10 +4,10 @@ export CLICOLOR=true
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 # is_mac && command_exists brew && command_exists android && export ANDROID_HOME=$(brew --prefix android-sdk)
 
-if command_exists rbenv; then
-    export RBENV_ROOT=/usr/local/var/rbenv
-    eval "$(rbenv init -)"
-fi
+# if command_exists rbenv; then
+    # export RBENV_ROOT=/usr/local/var/rbenv
+    # eval "$(rbenv init -)"
+# fi
 
 if command_exists nodenv; then
     export NODENV_ROOT=/usr/local/var/nodenv
@@ -15,7 +15,15 @@ if command_exists nodenv; then
 fi
 
 if command_exists pair; then
-  eval "$(pair --setup)"
+  pair() {
+    if gem list -i pair-up 2>/dev/null 1>/dev/null; then
+      command pair "$@"
+      if [[ -s "$HOME/.pair-up_export_authors" ]] ; then source "$HOME/.pair-up_export_authors" ; fi
+    else
+      echo "You do not have pair-up installed for your current ruby version."
+      echo "Please run $> gem install pair-up"
+    fi
+  }
 fi
 
 # if command_exists opam; then
