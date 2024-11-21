c97ca8884b2a7d28dea8207c070fd6808c675963
Kevin Smith <ksmithbaylor@gmail.com>
2024-11-21 11:41:57 -0500

    Get rid of old *env tool setup

--- .scripts/common/exports.sh
+++ .scripts/common/exports.sh
@@ -8,19 +8,3 @@ export ERL_AFLAGS="-kernel shell_history enabled"
 if command_exists cargo; then
   source "$HOME/.cargo/env"
 fi
-
-# if command_exists rbenv; then
-#     export RBENV_ROOT=/usr/local/var/rbenv
-#     eval "$(rbenv init -)"
-# fi
-#
-# if command_exists nodenv; then
-#     export NODENV_ROOT=/usr/local/var/nodenv
-#     eval "$(nodenv init -)"
-# fi
-#
-# export PYENV_ROOT="$HOME/.pyenv"
-# export PATH="$PYENV_ROOT/bin:$PATH"
-# if command_exists pyenv; then
-#     eval "$(pyenv init -)"
-# fi
