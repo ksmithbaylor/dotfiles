8394a8b1134b292acba7a4dcdd09988693091939
Kevin Smith <ksmithbaylor@gmail.com>
2026-06-18 16:33:39 -0400

    Update tickers swiftbar plugin

--- .config/swiftbar/tickers.5s.sh
+++ .config/swiftbar/tickers.5s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # <xbar.title>BTC Price</xbar.title>
 
-function ticker() {
+function coinbase() {
   SYMBOL=$1
   LABEL=$2
   VALUE=$(curl -s https://api.exchange.coinbase.com/products/$SYMBOL/ticker | jq -r .bid)
@@ -9,5 +9,18 @@ function ticker() {
   echo -ne $LABEL: \$$(LC_ALL=en_US numfmt --grouping $ROUNDED) '  '
 }
 
-ticker BTC-USD BTC
-ticker ETH-USD ETH
+function stock() {
+  SYMBOL=$1
+  LABEL=$1
+  RESPONSE=$(curl -s https://stockanalysis.com/api/quotes/s/$SYMBOL)
+  VALUE=$(echo ${RESPONSE} | jq .data.ep)
+  if [[ $VALUE == "null" ]]; then
+    VALUE=$(echo ${RESPONSE} | jq .data.p)
+  fi
+  ROUNDED=$(printf '%0.2f' $VALUE)
+  echo -ne $LABEL: \$$(LC_ALL=en_US numfmt --grouping $ROUNDED) '  '
+}
+
+coinbase BTC-USD BTC
+coinbase ETH-USD ETH
+stock CRCL
